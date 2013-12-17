262f8d4862d3d35d911c54db502974b76fda66f6
artbez <artemii.bezguzikov@gmail.com>
2013-12-17 16:54:08 +0400

    Поставил комментриии, улучшил читабельность кода

--- HW12/task1/task1.cpp
+++ HW12/task1/task1.cpp
@@ -5,12 +5,12 @@
 using std::cin;
 using std::cout;
 
-bool initial(int &indexOfStr, char *& str, const int length);
-bool preMul(int &indexOfStr, char *& str, const int length);
-bool sum(int &indexOfStr, char *& str, const int length);
-bool mul(int &indexOfStr, char *& str, const int length);
-bool preBrack(int &indexOfStr, char *& str, const int length);
-bool isNum(int &indexOfStr, char *& str, const int length);
+bool initial(int &indexOfStr, char *& str, const int length); // initial -> preMul sum
+bool preMul(int &indexOfStr, char *& str, const int length); // preMul -> preBrack mul
+bool sum(int &indexOfStr, char *& str, const int length); // sum -> (+|-) preMul sum | eps
+bool mul(int &indexOfStr, char *& str, const int length); // mul -> (*|/) preBrack mul | eps
+bool preBrack(int &indexOfStr, char *& str, const int length); // preBrack -> (initial) | eps
+bool isNum(int &indexOfStr, char *& str, const int length); // is this a number
 
 int main()
 {
@@ -27,19 +27,22 @@ int main()
 
 bool initial(int &indexOfStr, char *& str, const int length)
 {
-    if (indexOfStr == length) return true;
+    if (indexOfStr == length)
+        return true;
     return (preMul(indexOfStr, str, length) * sum(indexOfStr, str, length));
 }
 
 bool preMul(int &indexOfStr, char *& str, const int length)
 {
-    if (indexOfStr == length) return true;
+    if (indexOfStr == length)
+        return true;
     return (preBrack(indexOfStr, str, length) * mul(indexOfStr, str, length));
 }
 
 bool sum(int &indexOfStr, char *& str, const int length)
 {
-    if (indexOfStr == length) return true;
+    if (indexOfStr == length)
+        return true;
     int curIndex = indexOfStr;
     if ((str[indexOfStr] == '+') || (str[indexOfStr] == '-'))
     {
@@ -52,7 +55,8 @@ bool sum(int &indexOfStr, char *& str, const int length)
 
 bool mul(int &indexOfStr, char *& str, const int length)
 {
-    if (indexOfStr == length) return true;
+    if (indexOfStr == length)
+        return true;
     int curIndex = indexOfStr;
     if ((str[indexOfStr] == '*') || (str[indexOfStr] == '/'))
     {
